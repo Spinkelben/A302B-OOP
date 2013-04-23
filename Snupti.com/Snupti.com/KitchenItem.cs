15820329970c73885a437f55d177478730d59aaa
Emil <emil@nesgaard.dk>
2013-04-23 22:14:17 +0200

    Tiljøjede KitchenItem og NonKitchenItems klasser

---
+++ Snupti.com/Snupti.com/KitchenItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snupti.com
+{
+    abstract class KitchenItem : Item
+    {
+        public enum SmileySystem {Sur, Ligeglad, Glad};
+
+        public abstract SmileySystem Smiley
+        {
+            get;
+        }
+    }
+}   
