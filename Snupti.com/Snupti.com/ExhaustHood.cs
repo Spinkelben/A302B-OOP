e39604e72fc91b73053b4a7c2877d7dca236b0fc
kenneth <khch12@student.aau.dk>
2013-04-23 15:34:16 +0200

    added stuff

--- Snupti.com/Snupti.com/ExhaustHood.cs
+++ Snupti.com/Snupti.com/ExhaustHood.cs
@@ -14,6 +14,27 @@ namespace Snupti.com
         private int _suctionCapacity;
         // ja og nej
         private bool _filter;
+
+        public enum hoodtype
+        {
+            Frithængt,
+            Væghængt,
+            Indbygget
+        }
+
+        public string Type
+        {
+            get 
+            {
+                return _type;
+            }
+            set 
+            {
+                if ()
+                {
+                }
+            }
+        }
            
         public int SuctionCapacity 
         {
@@ -33,5 +54,34 @@ namespace Snupti.com
                 }
             }
         }
+
+        public static int GetKitchenSize(int suctionvalue) 
+        {
+            if (suctionvalue < 1300 ||suctionvalue > 875 && suctionvalue < 1300)
+            {
+                return 35;
+            }
+            if (suctionvalue > 750 && suctionvalue < 1100)
+            {
+                return 30;
+            }
+            if (suctionvalue > 625 && suctionvalue < 900)
+            {
+                return 25;
+            }
+            if (suctionvalue > 500 && suctionvalue < 750)
+            {
+                return 20;
+            }
+            if (suctionvalue > 375 && suctionvalue < 750)
+            {
+                return 15;
+            }
+            if (suctionvalue > 250 && suctionvalue < 500)
+            {
+                return 10;
+            }
+            return 10;
+        }  
     }
 }
