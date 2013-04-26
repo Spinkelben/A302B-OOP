df1722a411c0ea8fb3ac84a20ac9071c09baafbc
Søren Ranneries <sranne12@student.aau.dk>
2013-04-26 15:52:52 +0200

    Halløj men styring af inventaret.

---
+++ Snupti.com/Snupti.com/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snupti.com
+{
+    class Inventory
+    {
+        private List<Tuple<Item, int>> _stock = new List<Tuple<Item, int>>();
+
+        public void Add(Item item, int amount)
+        {
+            //Tjek om varen allerede findes i listen
+            var items = _stock.Where(s => s.Item1.Name == item.Name);
+            if (items.Count() > 1)
+            {
+                Console.WriteLine("WHAT!");
+            }
+            //Tilføj varen
+        }
+    }
+}
